b52a6b3bc6713e3d92741badc4e5c304367994ae
aayushsaini <aayush.r98@gmail.com>
2020-09-01 00:34:50 +0530

    SPA p-2 | Breadcrumb, Routers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 // constructor(props) {
 //     super(props);
@@ -62,6 +63,19 @@ const DishDetails = (props) => {
         // console.log('d: '+props.dish.description);
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row mt-2">
                     <div className="col-12 col-md-5 ml-1">
                         <Card>
@@ -70,7 +84,7 @@ const DishDetails = (props) => {
                         </Card>
                     </div>
                     <div className="col-12 col-md-5 ml-1">
-                        <RenderComments comments={props.dish.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             </div>
